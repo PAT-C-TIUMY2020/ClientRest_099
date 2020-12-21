3878bb43f0c5796f5da3525cde7f50b3957d74e2
ARGA APRILIAN PUTRA JAYA <arga.aprilian.ft18@mail.umy.ac.id>
2020-12-21 22:18:35 +0700

    Add project files.

---
+++ ClientRest_20180140099_Arga Aprilian Putra Jaya/ClassData.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientRest_20180140099_Arga_Aprilian_Putra_Jaya
+{
+    class ClassData
+    {
+        public void getData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            foreach (var mhs in data)
+            {
+                Console.WriteLine("Nama: " + mhs.nama);
+                Console.WriteLine("NIM: " + mhs.nim);
+                Console.WriteLine("Prodi: " + mhs.prodi);
+                Console.WriteLine("Angkatan: " + mhs.angkatan);
+            }
+            Console.ReadLine();
+        }
+    }
+}
